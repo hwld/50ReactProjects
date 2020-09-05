87b91800e4d40dc3dee18b82003c0a273e656266
hwld <0701hw@gmail.com>
2020-09-05 23:00:53 +0900

    [marketing_website] デバッグ

--- business_and_realworld/marketing_website/src/components/contact-form-prerender.tsx
+++ business_and_realworld/marketing_website/src/components/contact-form-prerender.tsx
@@ -6,8 +6,8 @@ export const ContactFormPrerender: React.FC<FormHTMLAttributes<
 >> = ({ ...props }) => {
   return (
     <form {...props} hidden>
-      <input type="hidden" name="form-name" value="contact" />
       <input name="name" />
+      <input name="email" />
       <input name="email" />
       <textarea name="comment" />
     </form>
