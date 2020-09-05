0fac110567f1fca1fa30c16fbf236608789d7e53
hwld <0701hw@gmail.com>
2020-09-05 23:09:28 +0900

    [marketing_website] デバッグ

--- business_and_realworld/marketing_website/src/components/contact-form-prerender.tsx
+++ business_and_realworld/marketing_website/src/components/contact-form-prerender.tsx
@@ -1,12 +1,15 @@
-import React, { FormHTMLAttributes } from "react";
+import React from "react";
 
 // A little help for the Netlify post-processing bots
-export const ContactFormPrerender: React.FC<FormHTMLAttributes<
-  HTMLFormElement
->> = ({ ...props }) => {
+export const ContactFormPrerender: React.FC = () => {
   return (
-    <form {...props} hidden>
-      <input name="name" />
+    <form
+      name="contact"
+      data-netlify="true"
+      data-netlify-honeypot="bot-hield"
+      hidden
+    >
+      <input name="name" hidden />
       <input name="email" />
       <input name="email" />
       <textarea name="comment" />
