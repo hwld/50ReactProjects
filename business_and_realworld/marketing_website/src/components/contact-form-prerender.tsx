48de08d96d5a744055a5b1f1b8cea1144b62a40b
hwld <0701hw@gmail.com>
2020-09-05 22:51:40 +0900

    [marketing_website] netlify formのデバッグ

--- business_and_realworld/marketing_website/src/components/contact-form-prerender.tsx
+++ business_and_realworld/marketing_website/src/components/contact-form-prerender.tsx
@@ -6,8 +6,9 @@ export const ContactFormPrerender: React.FC<FormHTMLAttributes<
 >> = ({ ...props }) => {
   return (
     <form {...props} hidden>
+      <input type="hidden" name="form-name" value="contact" />
       <input name="name" />
-      <input name="hoge" />
+      <input name="email" />
       <textarea name="comment" />
     </form>
   );
