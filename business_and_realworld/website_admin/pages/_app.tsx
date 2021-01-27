8bb7b8ae728b4c47151240f1d53d563cde1ef37d
hwld <0701hw@gmail.com>
2021-01-27 21:12:20 +0900

    [website_admin] react-queryの実験ページを追加

--- business_and_realworld/website_admin/pages/_app.tsx
+++ business_and_realworld/website_admin/pages/_app.tsx
@@ -1,12 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { theme } from "../theme";
+
+const client = new QueryClient();
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
---
+++ business_and_realworld/website_admin/pages/labo/index.tsx
@@ -0,0 +1,79 @@
+import { Button, Center, VStack, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+
+const fetcher = async () => {
+  return Math.floor(Math.random() * 1000);
+};
+
+const Child: React.FC = () => {
+  const { data } = useQuery("key", fetcher, {
+    refetchOnMount: false,
+    cacheTime: 1000,
+  });
+
+  console.log(`child: ${data}`);
+
+  return (
+    <VStack bg="gray.500" boxSize="300px" p={3} mt={3}>
+      <Text>Child: {data}</Text>
+    </VStack>
+  );
+};
+
+const Parent: React.FC = () => {
+  const { data } = useQuery("key", fetcher, {
+    refetchOnMount: false,
+    cacheTime: 1000,
+  });
+  const [isOpen, setIsOpen] = useState(false);
+
+  console.log(`parent: ${data}`);
+
+  return (
+    <VStack bg="gray.400" boxSize="400px" p={3} mt={3}>
+      <Text>Parent: {data}</Text>
+      <Button
+        onClick={() => {
+          setIsOpen((s) => !s);
+        }}
+      >
+        Click
+      </Button>
+      {isOpen && <Child />}
+    </VStack>
+  );
+};
+
+const New: React.FC = () => {
+  const { data } = useQuery("key", fetcher, {
+    refetchOnMount: false,
+    cacheTime: 1000,
+  });
+
+  console.log(`new: ${data}`);
+
+  return (
+    <VStack bg="gray.400" boxSize="400px" p={3} mt={3}>
+      <Text>New: {data}</Text>
+    </VStack>
+  );
+};
+
+export default function LaboPage(): JSX.Element {
+  const [isDisplay, setisDisplay] = useState(false);
+  const [isDisplay2, setIsDisplay2] = useState(false);
+
+  return (
+    <Center>
+      <VStack bg="gray.300" boxSize="500px" mt={10} p={3}>
+        <Button onClick={() => setisDisplay((s) => !s)}>CLICK</Button>
+        {isDisplay && <Parent />}
+      </VStack>
+      <VStack bg="gray.300" boxSize="500px" mt={10} ml={1} p={3}>
+        <Button onClick={() => setIsDisplay2((s) => !s)}>Click</Button>
+        {isDisplay2 && <New />}
+      </VStack>
+    </Center>
+  );
+}
