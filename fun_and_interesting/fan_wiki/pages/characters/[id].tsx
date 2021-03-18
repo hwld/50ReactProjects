b1d5028564a2968a58e594d7616e9075c1a585a3
hwld <0701hw@gmail.com>
2021-03-18 14:00:09 +0900

    [fan_wiki] キャラクタページを効率化

--- fun_and_interesting/fan_wiki/pages/characters/[id].tsx
+++ fun_and_interesting/fan_wiki/pages/characters/[id].tsx
@@ -1,37 +1,50 @@
-import React from "react";
-import { GetServerSideProps, NextPage } from "next";
+import React, { useEffect, useState } from "react";
+import { NextPage } from "next";
 import { Image, Text } from "@chakra-ui/react";
 import { Character, fetchCharacter } from "../../fetch";
+import { useCharacters } from "../../context/CharactersContext";
+import { useRouter } from "next/router";
 
 type Props = {
   character: Character;
 };
 
-const CharacterPage: NextPage<Props> = ({ character }) => {
+const CharacterPage: NextPage<Props> = ({}) => {
+  const characters = useCharacters();
+  const router = useRouter();
+  const { id } = router.query;
+
+  const [character, setCharacter] = useState<Character | undefined>(
+    characters.find((c) => c.id === id)
+  );
+
+  useEffect(() => {
+    async function fetch() {
+      if (typeof id !== "string") {
+        return;
+      }
+      const c = await fetchCharacter(id);
+      setCharacter(c);
+    }
+
+    if (!character) {
+      fetch();
+    }
+  }, [character, id]);
+
   return (
     <div>
-      <Image src={character.image} boxSize="300px" />
-      <Text>{character.name}</Text>
-      <Text>{character.status}</Text>
-      <Text>{character.species}</Text>
-      <Text>{character.gender}</Text>
+      {character && (
+        <>
+          <Image src={character.image} boxSize="300px" />
+          <Text>{character.name}</Text>
+          <Text>{character.status}</Text>
+          <Text>{character.species}</Text>
+          <Text>{character.gender}</Text>
+        </>
+      )}
     </div>
   );
 };
 
 export default CharacterPage;
-
-export const getServerSideProps: GetServerSideProps<Props> = async ({
-  params,
-}) => {
-  // ファイル名を[...id].tsxのようにしなければidに配列が入ることはない
-  const id = params.id as string;
-
-  let character: Character;
-  try {
-    character = await fetchCharacter(id);
-  } catch (error) {
-    return { notFound: true };
-  }
-  return { props: { character } };
-};
