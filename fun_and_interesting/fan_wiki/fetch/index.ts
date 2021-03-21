12b738e86396586ec86840c0a2fac9b8edfab522
hwld <0701hw@gmail.com>
2021-03-21 16:54:33 +0900

    [fan_wiki] キャラクタページにエピソードを表示

--- fun_and_interesting/fan_wiki/fetch/index.ts
+++ fun_and_interesting/fan_wiki/fetch/index.ts
@@ -1,10 +1,18 @@
+export type Episode = {
+  id: string;
+  name: string;
+};
+
 export type Character = {
   id: string;
   image: string;
   name: string;
   status: "Alive" | "Dead" | "unknown";
   species: string;
-  gender: "Female" | "Male" | "Genderless" | "unknown";
+  origin: { name: string };
+  location: { name: string };
+  // 最後の/にepisodeのidが続く
+  episode: string[];
 };
 
 export async function fetchCharacters(
@@ -13,14 +21,20 @@ export async function fetchCharacters(
   const res = await fetch(
     `https://rickandmortyapi.com/api/character/${[...ids]}`
   );
-
   if (!res.ok) {
     return undefined;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  let characters: any[] = await res.json();
-  characters = characters.map((c) => ({ ...c, id: c.id.toString() }));
+  type Fetched = Omit<Character, "id"> & { id: number };
+  let fetched: Fetched[] | Fetched = await res.json();
+  if (!Array.isArray(fetched)) {
+    fetched = [fetched];
+  }
+
+  const characters: Character[] = fetched.map((c) => ({
+    ...c,
+    id: c.id.toString(),
+  }));
 
   return characters;
 }
@@ -29,7 +43,6 @@ export async function fetchCharacter(
   id: string
 ): Promise<Character | undefined> {
   const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
-
   if (!res.ok) {
     return undefined;
   }
@@ -37,4 +50,28 @@ export async function fetchCharacter(
   let character = await res.json();
   character = { ...character, id: character.id.toString() };
   return character;
+}
+
+export async function fetchEpisodes(
+  ids: string[]
+): Promise<Episode[] | undefined> {
+  const res = await fetch(
+    `https://rickandmortyapi.com/api/episode/${[...ids]}`
+  );
+  if (!res.ok) {
+    return undefined;
+  }
+
+  type Fetched = Omit<Episode, "id"> & { id: number };
+  let fetched: Fetched[] | Fetched = await res.json();
+  if (!Array.isArray(fetched)) {
+    fetched = [fetched];
+  }
+
+  const episodes: Episode[] = fetched.map((e) => ({
+    ...e,
+    id: e.id.toString(),
+  }));
+
+  return episodes;
 }
--- fun_and_interesting/fan_wiki/pages/characters/[id].tsx
+++ fun_and_interesting/fan_wiki/pages/characters/[id].tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { NextPage } from "next";
-import NextImage from "next/image";
-import { Image, Text } from "@chakra-ui/react";
-import { Character, fetchCharacter } from "../../fetch";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Character, Episode, fetchCharacter, fetchEpisodes } from "../../fetch";
 import { useCharacters } from "../../context/CharactersContext";
 import { useRouter } from "next/router";
+import { Image } from "../../components/Image";
+import { CharacterStatusIcon } from "../../components/CharacterStatusIcon";
 
 type Props = {
   character: Character;
@@ -18,33 +19,94 @@ const CharacterPage: NextPage<Props> = ({}) => {
   const [character, setCharacter] = useState<Character | undefined>(
     characters.find((c) => c.id === id)
   );
+  const [episodes, setEpisodes] = useState<Episode[]>([]);
 
+  // グローバルステートにキャラクタ情報がなければキャラクタ情報を読み込む
   useEffect(() => {
-    async function fetch() {
-      if (typeof id !== "string") {
-        return;
-      }
-      const c = await fetchCharacter(id);
-      setCharacter(c);
+    if (character || typeof id !== "string") {
+      return;
     }
-
-    if (!character) {
-      fetch();
-    }
+    fetchCharacter(id).then((c) => setCharacter(c));
   }, [character, id]);
 
+  // キャラクターが登場したエピソードを読み込む
+  useEffect(() => {
+    if (episodes.length !== 0 || !character) {
+      return;
+    }
+    const episodeIds = character.episode.map((episodeUrl) => {
+      const paths = episodeUrl.split("/").filter((s) => Boolean(s));
+      return paths[paths.length - 1];
+    });
+
+    fetchEpisodes(episodeIds).then((episodes) => {
+      if (!episodes) {
+        return;
+      }
+      setEpisodes(episodes);
+    });
+  }, [character]);
+
   return (
-    <div>
+    <Box overflow="auto">
+      <Box
+        bg="gray.500"
+        h="30vh"
+        w="100%"
+        pos="absolute"
+        top={0}
+        left={0}
+        zIndex={-1}
+      />
       {character && (
-        <>
-          <NextImage src={character.image} width={300} height={300} />
-          <Text>{character.name}</Text>
-          <Text>{character.status}</Text>
-          <Text>{character.species}</Text>
-          <Text>{character.gender}</Text>
-        </>
+        <Box w="800px" maxW="100%" m="auto" mt="180px">
+          <Flex align="center">
+            <Image
+              src={character.image}
+              imageWidth={300}
+              imageHeight={300}
+              borderRadius="50%"
+              flexShrink={0}
+              alignSelf="flex-start"
+            />
+            <Box ml={10} minW={0}>
+              <Heading size="3xl">{character.name}</Heading>
+              <Flex align="center" ml={3} mt={3}>
+                <CharacterStatusIcon
+                  boxSize="0.8rem"
+                  status={character.status}
+                  flexShrink={0}
+                  mr={1}
+                />
+                <Text minW={0} fontWeight="bold" fontSize="xl">
+                  {character.status} - {character.species}
+                </Text>
+              </Flex>
+            </Box>
+          </Flex>
+          <Box mt={5}>
+            <Heading>Origin location</Heading>
+            <Text ml={3} fontSize="xl">
+              {character.origin.name}
+            </Text>
+
+            <Heading mt={3}>Last known location</Heading>
+            <Text ml={3} fontSize="xl">
+              {character.location.name}
+            </Text>
+
+            <Heading mt={3}>Episodes that appeared</Heading>
+            {episodes.map((episode) => (
+              <Text
+                key={episode.id}
+                ml={3}
+                fontSize="xl"
+              >{`Episode${episode.id} - ${episode.name}`}</Text>
+            ))}
+          </Box>
+        </Box>
       )}
-    </div>
+    </Box>
   );
 };
 
