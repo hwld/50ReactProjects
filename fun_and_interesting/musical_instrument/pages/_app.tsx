7fff55df20b5b6b3b12d534acd6130085596819a
hwld <0701hw@gmail.com>
2021-04-03 20:31:18 +0900

    [musical_instrument] init

---
+++ fun_and_interesting/musical_instrument/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import { AppProps } from "next/dist/next-server/lib/router/router";
+
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
