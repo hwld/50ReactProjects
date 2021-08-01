5a68342b5b4409a24d0508194c9fba59119afebd
hwld <0701hw@gmail.com>
2021-08-01 17:06:54 +0900

    [survey] menuの位置を動的に設定

--- business_and_realworld/survey_creator_and_manager/components/pages/edit/SurveyEditor.tsx
+++ business_and_realworld/survey_creator_and_manager/components/pages/edit/SurveyEditor.tsx
@@ -1,7 +1,7 @@
 import { AddIcon } from "@chakra-ui/icons";
 import { Box, Button, Center, Flex, IconButton } from "@chakra-ui/react";
 import { useRouter } from "next/dist/client/router";
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSurvey } from "../../../hooks/useSurvey";
 import { Survey } from "../../../type/survey";
 import { Header } from "../../common/Header";
@@ -13,6 +13,12 @@ type Props = { initialSurvey: Survey };
 const Component: React.FC<Props> = ({ initialSurvey }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const appHeaderHeight = 70;
+  const editorTopMargin = 20;
+  const headerInputTop = appHeaderHeight + editorTopMargin;
+
+  const [menuTop, setMenuTop] = useState(0);
+  const menuHeight = 300;
 
   const {
     survey,
@@ -23,6 +29,37 @@ const Component: React.FC<Props> = ({ initialSurvey }) => {
     changeItem,
   } = useSurvey(initialSurvey);
 
+  const convertMenuViewTopToMenuTop = useCallback(
+    (viewTop: number) => {
+      const menuViewBottom = viewTop + menuHeight;
+      // headerInputのtopがmenuTopの0のため、それを引く
+      let newMenuTop = viewTop + window.scrollY - headerInputTop;
+
+      // 上にはみ出す場合
+      if (viewTop < headerInputTop) {
+        newMenuTop = window.scrollY;
+      }
+
+      //下にはみ出す場合
+      if (menuViewBottom > window.innerHeight - editorTopMargin) {
+        newMenuTop =
+          newMenuTop -
+          (menuViewBottom - (window.innerHeight - editorTopMargin));
+      }
+
+      return newMenuTop;
+    },
+    [headerInputTop]
+  );
+
+  const handleFocus: React.FocusEventHandler<HTMLDivElement> = ({
+    currentTarget,
+  }) => {
+    const menuViewTop = currentTarget.getBoundingClientRect().top;
+    const newMenuTop = convertMenuViewTopToMenuTop(menuViewTop);
+    setMenuTop(newMenuTop);
+  };
+
   const handleCreateSurvey = async () => {
     await fetch(`/api/surveys/${survey.id}`, {
       method: "PUT",
@@ -32,9 +69,28 @@ const Component: React.FC<Props> = ({ initialSurvey }) => {
     router.push("/");
   };
 
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+
+    const handleScroll = () => {
+      clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        setMenuTop((top) => {
+          const menuViewTop = headerInputTop + top - window.scrollY;
+          return convertMenuViewTopToMenuTop(menuViewTop);
+        });
+      }, 50);
+    };
+
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, [convertMenuViewTopToMenuTop, headerInputTop]);
+
   return (
     <Box minH="100vh" bgColor="gray.600">
-      <Header justifyContent="flex-end">
+      <Header justifyContent="flex-end" h={`${appHeaderHeight}px`}>
         <Button
           mt={3}
           mr={3}
@@ -47,9 +103,15 @@ const Component: React.FC<Props> = ({ initialSurvey }) => {
           調査を作成
         </Button>
       </Header>
-      <Flex mt={10} mx="auto" w="800px">
+      <Flex mt={`${editorTopMargin}px`} mx="auto" w="800px">
         <Box flexGrow={1}>
-          <Box p={10} bgColor="gray.700" borderRadius="10px" boxShadow="md">
+          <Box
+            p={10}
+            bgColor="gray.700"
+            borderRadius="10px"
+            boxShadow="md"
+            onFocus={handleFocus}
+          >
             <SurveyHeaderInput
               title={survey.title}
               description={survey.description}
@@ -66,18 +128,23 @@ const Component: React.FC<Props> = ({ initialSurvey }) => {
                 onChangeItem={changeItem}
                 onDeleteItem={deleteItem}
                 setError={setError}
+                borderWidth="2px"
+                borderColor="transparent"
+                _focusWithin={{ borderColor: "blue.500" }}
+                onFocus={handleFocus}
               />
             );
           })}
         </Box>
+
         <Box
           ml={5}
           p={3}
           bgColor="gray.300"
           w="50px"
-          h="300px"
-          position="sticky"
-          top="80px"
+          h={`${menuHeight}px`}
+          position="relative"
+          top={`${menuTop}px`}
           borderRadius="10px"
         >
           <Center>
